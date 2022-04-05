44fdf53ec9e8112b82df9b44a044724e734732e8
angedc02 <angedc02@gmail.com>
2022-04-05 19:00:55 +0800

    ange-admin-area
    fix admin-area

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -15,6 +15,8 @@ import { AdminLevelComponent } from './admin-levels/admin-level/admin-level.comp
 import { AdminCreateAccountComponent } from './admin-create-account/admin-create-account.component';
 import { AdminChangePassComponent } from './admin-change-pass/admin-change-pass.component';
 import { AdminSummaryGenComponent } from './admin-summary-gen/admin-summary-gen.component';
+import { AdminAreaComponent } from './admin-areas/admin-area/admin-area.component';
+import { AdminAreasComponent } from './admin-areas/admin-areas.component';
 
 
 @NgModule({
@@ -32,7 +34,9 @@ import { AdminSummaryGenComponent } from './admin-summary-gen/admin-summary-gen.
     AdminHomeComponent,
     AdminCreateAccountComponent,
     AdminChangePassComponent,
-    AdminSummaryGenComponent
+    AdminSummaryGenComponent,
+    AdminAreaComponent,
+    AdminAreasComponent
   ],
   imports: [
     CommonModule,
