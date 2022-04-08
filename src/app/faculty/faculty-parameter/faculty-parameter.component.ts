05d2387a6692ce4801e4e0b90614ab6b71ad65ef
Nyanyer <cuachin.haroldjames.p.2064@gmail.com>
2022-04-08 12:52:58 +0800

    Harold-faculty-parameter

---
+++ src/app/faculty/faculty-parameter/faculty-parameter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-parameter',
+  templateUrl: './faculty-parameter.component.html',
+  styleUrls: ['./faculty-parameter.component.css']
+})
+export class FacultyParameterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
